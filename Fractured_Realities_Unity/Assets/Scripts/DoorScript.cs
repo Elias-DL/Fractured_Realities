24fd4a9bec0c9e569a945722044131a096e82d11
aynurnadim <aynur.nadim@leerling.campushast.be>
2025-01-06 22:26:02 +0100

    instellenTP

--- Fractured_Realities_Unity/Assets/Scripts/DoorScript.cs
+++ Fractured_Realities_Unity/Assets/Scripts/DoorScript.cs
@@ -4,18 +4,6 @@ using UnityEngine.SceneManagement;
 public class Door : MonoBehaviour
 {
     public string doorName; // Unique name for the door.
-    public string targetScene; // The name of the target scene.
-    public Vector3 targetSpawnPoint; // Position in the target scene to spawn at.
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            // Save the door name and target spawn point to TeleportManager.
-            TeleportManager.Instance.SetLastDoorUsed(doorName, targetSpawnPoint);
-
-            // Load the target scene.
-            SceneManager.LoadScene(targetScene);
-        }
-    }
+    
+    
 }
