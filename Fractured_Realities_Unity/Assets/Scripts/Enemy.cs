9f020f0be491a2fd2063d34cc27f4dc6d65b3993
Elias-DL <eliaslenchoj.deloore@leerling.campushast.be>
2025-02-04 19:37:30 +0100

    Morgen easy wiskunde W

--- Fractured_Realities_Unity/Assets/Scripts/Enemy.cs
+++ Fractured_Realities_Unity/Assets/Scripts/Enemy.cs
@@ -15,17 +15,17 @@ public class ZombieAI : MonoBehaviour // reset de component voor changes
         Zone3      // Zone 3 (do nothing here)
     }
 
-    public Zone currentZone = Zone.None;  // Start outside of any zone
-    public Transform playertrans;              // The player the zombie will chase
-    public float detectionRadius = 50;   // Radius for detection
-    public float roamRange = 100;         // Maximum roaming range from current position
-    public float stopChaseDistance = 20; // Distance at which the zombie stops chasing the player
+    //public Zone currentZone = Zone.None;  
+    public Transform playerTrans;             
+    public float detectionRadius = 50;   
+    public float roamRange = 100;         
+    public float stopChaseDistance = 20; 
     private NavMeshAgent navAgent;
-    private Animator animator; // Animator reference
+    private Animator animator; 
     private Vector3 startPOS;
     private string action;
-    private bool exitedZone = false;
-    public Transform startZone1;
+    //private bool exitedZone = false;
+    //public Transform startZone1;
     public float damage;
     public GameObject Player;
     public void Awake()
@@ -34,7 +34,7 @@ public class ZombieAI : MonoBehaviour // reset de component voor changes
         Player = GameObject.FindWithTag("Player");
 
 
-        playertrans = Player.transform;
+        playerTrans = Player.transform;
     }
 
 
@@ -50,14 +50,14 @@ public class ZombieAI : MonoBehaviour // reset de component voor changes
     private void Update()
     {
         Debug.Log(action);
-        if (currentZone == Zone.Zone1)
+        //if (currentZone == Zone.Zone1)
 
-        {
+        //{
             animator.SetBool("Roam", true);
             animator.SetBool("Chase", false);
             animator.SetBool("Attack", false);
 
-            float distanceToPlayer = Vector3.Distance(transform.position, playertrans.position);
+            float distanceToPlayer = Vector3.Distance(transform.position, playerTrans.position);
             //Debug.Log("Distance to player: " + distanceToPlayer + " detectionradius : " + detectionRadius);
 
             if (distanceToPlayer < detectionRadius)
@@ -72,13 +72,13 @@ public class ZombieAI : MonoBehaviour // reset de component voor changes
 
                 RoamAround();
             }
-        }
-        else
-        {
-            // Stop animations when outside Zone 1
+        //}
+        //else
+        //{
+        //    // Stop animations when outside Zone 1
            
-            navAgent.isStopped = true;
-        }
+        //    navAgent.isStopped = true;
+        //}
     }
 
 
@@ -112,7 +112,7 @@ public class ZombieAI : MonoBehaviour // reset de component voor changes
 
         float attackRange = 40f; 
 
-        float distanceToPlayer = Vector3.Distance(transform.position, playertrans.position);
+        float distanceToPlayer = Vector3.Distance(transform.position, playerTrans.position);
 
         if (distanceToPlayer <= attackRange)
         {
@@ -125,7 +125,7 @@ public class ZombieAI : MonoBehaviour // reset de component voor changes
 
             //Player.GetComponent<PlayerStats>().TakeDamage(damage); attack wordt te vaak gecontroleerd -> te snel geen health meer
             // Make the zombie face the player
-            Vector3 directionToPlayer = (playertrans.position - transform.position).normalized;
+            Vector3 directionToPlayer = (playerTrans.position - transform.position).normalized;
             Quaternion lookRotation = Quaternion.LookRotation(new Vector3(directionToPlayer.x, 0, directionToPlayer.z));
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
 
@@ -140,8 +140,8 @@ public class ZombieAI : MonoBehaviour // reset de component voor changes
             animator.SetBool("Roam", false);
             animator.SetBool("Attack", false);
 
-            Vector3 directionToPlayer = (playertrans.position - transform.position).normalized;
-            Vector3 stoppingPoint = playertrans.position - directionToPlayer * attackRange;
+            Vector3 directionToPlayer = (playerTrans.position - transform.position).normalized;
+            Vector3 stoppingPoint = playerTrans.position - directionToPlayer * attackRange;
 
             navAgent.isStopped = false;
             navAgent.SetDestination(stoppingPoint);
@@ -170,27 +170,27 @@ public class ZombieAI : MonoBehaviour // reset de component voor changes
     }
 
 
-    private void OnTriggerExit(Collider other)
-    { // 2 colliders?, timer is niet consistent
-        if (!other.CompareTag("Zone1") && !exitedZone)
-        {
-            exitedZone = true;
-            animator.SetBool("Roam", true); 
-            animator.SetBool("Attack", false);
-            animator.SetBool("Chase", false);
+    //private void OnTriggerExit(Collider other)
+    //{ // 2 colliders?, timer is niet consistent
+    //    if (!other.CompareTag("Zone1") && !exitedZone)
+    //    {
+    //        exitedZone = true;
+    //        animator.SetBool("Roam", true); 
+    //        animator.SetBool("Attack", false);
+    //        animator.SetBool("Chase", false);
 
-            currentZone = Zone.Zone1;
-            Debug.Log("exit zone1");
-            Debug.Log("go to " + startPOS);
+    //        currentZone = Zone.Zone1;
+    //        Debug.Log("exit zone1");
+    //        Debug.Log("go to " + startPOS);
 
-            //navAgent.isStopped = false;
-            navAgent.SetDestination(startPOS);
-            // probleem als chase of attack erna is wod de zombie 'afgeleid'
-        }
+    //        //navAgent.isStopped = false;
+    //        navAgent.SetDestination(startPOS);
+    //        // probleem als chase of attack erna is wod de zombie 'afgeleid'
+    //    }
 
 
 
-    }
+    //}
 
 
 
