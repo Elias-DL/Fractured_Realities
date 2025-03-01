636160ed24da013ef355e89c3a5c612f57a0fc79
aynurnadim <aynur.nadim@leerling.campushast.be>
2025-03-01 14:19:59 +0100

    donnydjankerroom3

---
+++ Fractured_Realities_Unity/Assets/Scripts/GuessingGame.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuessingGame : MonoBehaviour
+{
+    public TMP_InputField guessInput;
+    public TMP_Text feedbackText;
+    private int objRandomNr;
+
+    void Start()
+    {
+        objRandomNr = Random.Range(1, 101);
+        feedbackText.text = "Guess a number between 1 and 100!";
+    }
+
+    public void CheckGuess()
+    {
+        int playerGuess;
+        if (int.TryParse(guessInput.text, out playerGuess))
+        {
+            if (playerGuess < objRandomNr)
+            {
+                feedbackText.text = "Too LOW LOSER! Try again.";
+            }
+            else if (playerGuess > objRandomNr)
+            {
+                feedbackText.text = "Too HIGH LOSER! Try again.";
+            }
+            else
+            {
+                feedbackText.text = "Correct! You guessed it!";
+            }
+        }
+        else
+        {
+            feedbackText.text = "Please enter a valid number!";
+        }
+
+        guessInput.text = ""; //reset iput
+    }
+}
