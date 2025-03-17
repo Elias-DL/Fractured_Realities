351b6c89aca836b9167ae5fbe83cd80932cb147d
Elias-DL <eliaslenchoj.deloore@leerling.campushast.be>
2025-03-17 19:58:24 +0100

    Muuuusic

--- Fractured_Realities_Unity/Assets/Luik.cs
+++ Fractured_Realities_Unity/Assets/Luik.cs
@@ -7,11 +7,12 @@ public class Luik : MonoBehaviour
 
     public GameObject LuikDeur;
     public GameObject SPBarrels;
-
+    public GameObject Valve;
     void Start()
     {
         LuikDeur = GameObject.FindWithTag("LuikDeur");
         SPBarrels = GameObject.FindWithTag("SPBarrels");
+        Valve = GameObject.FindWithTag("Valve");
     }
 
     // Update is called once per frame
@@ -25,6 +26,6 @@ public class Luik : MonoBehaviour
         Debug.Log("close");
         LuikDeur.transform.position =  new Vector3(92, 51,178);
         SPBarrels.GetComponent<SpawnBarrels>().LuikBool = false;
-
+        Valve.transform.rotation = Quaternion.Euler(90f,0f,0f);
     }
 }
